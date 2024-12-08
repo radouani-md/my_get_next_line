638d1b18ed0b58fc178be285fc49c9832a97afbd
Mohammed Radouani <mradouan@e1r9p5.1337.ma>
2024-12-08 23:34:55 +0100

    my_get_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,38 +6,31 @@
 /*   By: mradouan <mradouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 13:59:30 by mradouan          #+#    #+#             */
-/*   Updated: 2024/12/07 23:30:32 by mradouan         ###   ########.fr       */
+/*   Updated: 2024/12/08 22:01:44 by mradouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*check_read(int fd, char *buffer)
+char	*check_read(int fd, char *buffer, char *static_v)
 {
-	char	*temp;
 	char	*joined;
 	ssize_t	read_bytes;
 
-	if (BUFFER_SIZE >= INT_MAX)
-		return (NULL);
-	temp = malloc(BUFFER_SIZE + 1);
-	if (!temp)
-		return (NULL);
-	read_bytes = read(fd, temp, BUFFER_SIZE);
-	while (read_bytes > 0)
+	while (1)
 	{
-		temp[read_bytes] = '\0';
-		joined = ft_strjoin(buffer, temp);
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes <= 0)
+    		break ;
+		buffer[read_bytes] = '\0';
+		joined = ft_strjoin(static_v, buffer);
 		if (!joined)
-			return (free(temp), free(buffer), NULL);
-		free(buffer);
-		buffer = joined;
-		if (!buffer || ft_strchr(buffer, '\n'))
+			return (free(buffer), free(static_v), NULL);
+		static_v = joined;
+		if (ft_strchr(static_v, '\n'))
 			break ;
-		read_bytes = read(fd, temp, BUFFER_SIZE);
 	}
-	free(temp);
-	return (buffer);
+	return (static_v);
 }
 
 char	*get_next(char *buffer)
@@ -46,8 +39,6 @@ char	*get_next(char *buffer)
 	int		i;
 
 	i = 0;
-	if (!buffer)
-		return (NULL);
 	while (buffer[i])
 	{
 		if (buffer[i] == '\n')
@@ -63,42 +54,55 @@ char	*get_next(char *buffer)
 
 char	*get_the_line(char *buffer)
 {
-	char	*retu;
 	int		i;
+	char	*str;
 
 	i = 0;
-	while (buffer[i] != '\0')
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
 	{
-		if (buffer[i] == '\n')
-			break ;
+		str[i] = buffer[i];
 		i++;
 	}
-	retu = malloc(i + 2);
-	if (!retu)
-		return (NULL);
-	ft_strncpy(retu, buffer, i);
 	if (buffer[i] == '\n')
-		retu[i++] = '\n';
-	retu[i] = '\0';
-	return (retu);
+	{
+		str[i] = buffer[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
+	static char	*static_v;
+	char	*buffer;
 	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
 	{
-		if (buffer)
-			free(buffer);
-		buffer = NULL;
+		if (static_v)
+			free(static_v);
+		static_v = NULL;
 		return (NULL);
 	}
-	buffer = check_read(fd, buffer);
+	if (BUFFER_SIZE >= INT_MAX)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	line = get_the_line(buffer);
-	buffer = get_next(buffer);
+	static_v = check_read(fd, buffer, static_v);
+	free(buffer);
+	if (!static_v)
+		return (free(static_v) , NULL);
+	line = get_the_line(static_v);
+	static_v = get_next(static_v);
 	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: mradouan <mradouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 14:47:10 by mradouan          #+#    #+#             */
-/*   Updated: 2024/12/07 23:25:08 by mradouan         ###   ########.fr       */
+/*   Updated: 2024/12/08 23:33:19 by mradouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,8 +67,6 @@ size_t	ft_strlen(char *s)
 {
 	size_t	i;
 
-	if (!s)
-		return (0);
 	i = 0;
 	while (s[i])
 		i++;
@@ -77,19 +75,29 @@ size_t	ft_strlen(char *s)
 
 char	*ft_strjoin(char *s1, char *s2)
 {
-	char	*result;
-	size_t	len1, len2;
+	size_t	lens;
+	size_t	i;
+	size_t	j;
+	char	*str;
 
+	i = 0;
+	j = 0;
+	if (!s1 && !s2)
+		return (NULL);
 	if (!s1)
 		return (ft_strdup(s2));
 	if (!s2)
 		return (ft_strdup(s1));
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	result = malloc(len1 + len2 + 1);
-	if (!result)
-		return (NULL);
-	ft_strncpy(result, s1, len1);
-	ft_strncpy(result + len1, s2, len2 + 1);
-	return (result);
+	lens = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(lens + 1);
+	if (!str)
+		return (free(s1), NULL);
+	while (s1[j] != '\0')
+		str[i++] = s1[j++];
+	j = 0;
+	while (s2[j] != '\0')
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	
+	return (free(s1), str);
 }
